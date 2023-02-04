5418336b5822ae0017d35bbfe95352bf7eb772b2
Thomas Aleknovic <aleknovic70@gmail.com>
2023-02-04 09:08:30 -0300

    Initial commit

---
+++ crud-spring/src/main/java/com/thomasaleknovic/crudspring/CrudSpringApplication.java
@@ -0,0 +1,30 @@
+package com.thomasaleknovic.crudspring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.thomasaleknovic.crudspring.model.Course;
+import com.thomasaleknovic.crudspring.repository.CourseRepository;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com spring");
+			c.setCategory("front-end");
+			
+			courseRepository.save(c); 
+		};
+	} 
+}
---
+++ crud-spring/src/main/java/com/thomasaleknovic/crudspring/controller/CourseController.java
@@ -0,0 +1,28 @@
+package com.thomasaleknovic.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thomasaleknovic.crudspring.model.Course;
+import com.thomasaleknovic.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+    
+    private CourseRepository courseRepository;
+    
+ 
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+    
+}
