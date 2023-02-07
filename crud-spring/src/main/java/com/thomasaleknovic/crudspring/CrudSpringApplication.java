16dca140beb0045d241ac9608b42287cb403f7a0
Thomas Aleknovic <aleknovic70@gmail.com>
2023-02-07 18:25:27 -0300

    fix: add h2 db

--- crud-spring/src/main/java/com/thomasaleknovic/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/thomasaleknovic/crudspring/CrudSpringApplication.java
@@ -15,6 +15,16 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
 
-	
+			Course c = new Course();
+			c.setName("Angular com spring");
+			c.setCategory("Front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
